4821038667d74d2ea2664bcb306e80f630d42327
TomLayan1 <afolayantomisin1919@gmail.com>
2025-04-22 15:30:58 +0100

    Features section renamed to categories, and made responsive

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const About:React.FC = () => {
   return (
     <section className='container mx-auto pt-20 pb-10'>
-      <div className='w-[70%] mx-auto'>
-        <h4 className='text-center font-bold tracking-widest mb-12'>ABOUT US</h4>
-        <h1 className='text-center text-3xl uppercase font-bold leading-11'>
+      <div className='w-[90%] md:w-[80%] lg:w-[70%] mx-auto'>
+        <h4 className='text-center font-bold tracking-widest mb-7 md:mb-12'>ABOUT US</h4>
+        <h1 className='text-center md:text-2xl lg:text-3xl uppercase font-bold leading-11'>
           At Velra, we bring comfort, style, and quality straight to your doorstep. As a modern furniture eCommerce brand, we make it easy to find pieces that fit your space, <span className='text-gray-400'>your taste, and your lifestyle.
           From timeless classics to contemporary designs, every item in our collection is carefully selected for durability, functionality, and beauty.</span></h1>
       </div>
--- src/Components/Features.tsx
+++ src/Components/Features.tsx
@@ -7,21 +7,21 @@ const Features:React.FC = () => {
     <section className='container mx-auto pt-10 pb-10'>
       <div>
         <h4 className='text-center font-bold tracking-widest mb-12'>FEATURES</h4>
-        <div className='w-[90%] mx-auto flex h-[250px]'>
-          <div className='flex gap-4'>
-            {feature?.map(item => (
-              <div key={item.id} className='relative overflow-hidden rounded-3xl'>
+        <div className='w-[90%] mx-auto lg:flex  h-[250px]'>
+          <div className='w-full lg:w-auto md:w-auto flex gap-4 mb-4 lg:mb-0 mr-4'>
+            {feature?.map((item, index) => (
+              index !== 2 && <div key={item.id} className='w-[50%] lg:w-auto relative overflow-hidden rounded-3xl'>
                 <img src={item.img} alt={item.name} className='hover:scale-110 duration-300' />
-                <p className='absolute z-10 right-3.5 bottom-3.5 bg-white p-2.5 rounded-full text-[14px]'>{item.name}</p>
+                <p className='absolute z-10 right-3.5 bottom-3.5 bg-white p-1.5 md:p-2.5 rounded-full text-[14px]'>{item.name}</p>
               </div>
             ))}
           </div>
-          <div className='flex flex-col gap-4'>
+          <div className='flex lg:flex-col gap-4'>
             {feature?.map(item => (
               item.imgs?.map(item => (
-                <div key={item.id} className='relative overflow-hidden rounded-3xl'>
+                <div key={item.id} className='lg:w-auto relative overflow-hidden rounded-3xl'>
                   <img src={item.img} className='hover:scale-110 duration-300' />
-                  <p className='absolute z-10 right-3.5 bottom-3.5 bg-white p-2.5 rounded-full text-[14px]'>{item.name}</p>
+                  <p className='absolute z-10 right-3.5 bottom-3.5 bg-white p-1.5 md:p-2.5 rounded-full text-[14px]'>{item.name}</p>
                 </div>
               ))
             ))}
